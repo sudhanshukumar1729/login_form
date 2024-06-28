127d5199e6e89e2b2953d24e1cf60732463bf4f8
Sudhanshu Kumar <122782163+sudhanshukumar1729@users.noreply.github.com>
2024-06-28 18:50:31 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,32 +3,13 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         if($username=='sudhanshu'&&$password=='12345'){
-            echo "user login";
+            echo "user login<br>";
+            echo $username;
         }
         else{
             echo "invalid user";
         }
 
-        $host = "localhost";
-        $dbusername = "root";
-        $dbpassword = "";
-        $dbname = "login";
-
-        $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
-        if($conn->connect_error){
-            echo "connection failed";
-        }
-
-        $stmt = $conn->prepare("SELECT * FROM login WHERE username='$username' AND password='$password'");
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if($result->num_rows==1){
-            echo "login successfull";
-        }
-        else{
-            echo "invalid user";
-        }
         
     }
 ?>
